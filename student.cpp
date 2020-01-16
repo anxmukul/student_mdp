d6a91fad0475798af3108d1a64cef509bd61b837
Mukul <kmukul1197@gmail.com>
2020-01-16 22:26:55 +0530

    Adding objects and accesing data members and defining data members inside the class

--- student.cpp
+++ student.cpp
@@ -5,26 +5,48 @@ class Student
     //Access specifier
     public:
     //Data members
-    string name, standard, roll_no, section;
+    string name; 
+    int standard; 
+    int roll_no;
+    char section;
     //Member functions
-    
+    void print_details()
+    {
+        cout<<"Name:"<<name<<endl<<"standard:"<<standard<<endl<<"roll no:"<<roll_no<<endl<<"section:"<<section<<endl;
+
+    }
 
 };
 
 int main()
 {
-    cout<<"This programme is a menudriven programme in which you will enter any number between 1-5 for following operation\n";
+    cout<<"This programme is a menu driven program in which you will enter any number between 1-5 for following operation\n";
     cout<<"1. For adding a new student.\n2. For listing all the students.\n3. For searching student by name.\n4. For seaching student by roll.\n5. To stop the programme.\n";
     
+    cout<<"Enter name of the student\n";
+    Student student1;    //declaring an object
+    student1.name = "Mukul";    //Accesing data member
+    student1.standard = 10;
+    student1.roll_no = 18;
+    student1.section = 'C';
+
     while(1>0){
         cout<<"Enter any number\n";
         int n;
         cin>>n;
         if (n==1){
             cout<<"Enter name of the student\n";
+            //Student student1;    //declaring an object
+            //student1.name = "Mukul";    //Accesing data member
+            //student1.standard = 10;
+            //student1.roll_no = 18;
+            //student1.section = 'C';
         }
         else if(n==2){
-            cout<<"You can get list of all student here\n";
+            //cout<<"You can get list of all student here\n";
+            //accesing member function
+            student1.print_details();
+
         }
         else if(n==3){
             cout<<"Enter name of the student.\n";
